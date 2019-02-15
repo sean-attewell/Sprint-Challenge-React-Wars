87bca6ad859b68475ddae32b423986525d047a4f
sean-attewell <sean.attewell@gmail.com>
2019-02-15 09:45:29 +0000

    styling on each character

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -3,8 +3,16 @@ import './Character.css';
 
 const Character = props => {
     return (
-        <div className="character">
-            {props.character.name}
+        <div className="characterContainer">
+            <li className="character">
+                {props.character.name} 
+            </li>
+            <div className="attr">
+            Hair color: {props.character.hair_color} 
+            </div>
+            <div className="attr">
+            Height: {props.character.height} 
+            </div>
         </div>
     );
 };
